a0e9b2c8e9025cd0c892230b4e6f8e271a167e85
Ranveer Shah <ranveer2001s@gmail.com>
2021-10-13 18:02:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ function loadData(records = []) {
 	var table_data = "";
 	for(let i=0; i<records.length; i++) {
 		table_data += `<tr>`;
-		table_data += `<td>${records[i].userId}</td>`;
+		table_data += `<td>${records[i].employeeCode}</td>`;
 		table_data += `<td>${records[i].jobTitleName}</td>`;
 		table_data += `<td>${records[i].firstName}</td>`;
-		table_data += `<td>${records[i].lastName}</td>`;
+		
 		table_data += `<td>${records[i].department}</td>`;
-		table_data += `<td>${records[i].employeeCode}</td>`;
+		
 		table_data += `<td>${records[i].phoneNumber}</td>`;
 		table_data += `<td>${records[i].emailAddress}</td>`;
+		table_data += `<td>${records[i].password}</td>`;
 		table_data += `<td>`;
 		table_data += `<a href="edit.html?id=${records[i]._id}"><i class="fas fa-edit"></i></a>  <i class="fas fa-trash-alt" onclick=deleteData('${records[i]._id}')></i>`;
 		table_data += `</td>`;
@@ -42,37 +43,35 @@ function getDataById(id) {
 		console.log(data);
 	
 		document.getElementById("id").value = data._id;
-		document.getElementById("userId").value = data.userId;
-		document.getElementById("firstName").value = data.firstName;
-		document.getElementById("lastName").value = data.lastName;
-		document.getElementById("phoneNumber").value = data.phoneNumber;
-		
-		
-		
-		document.getElementById("emailAddress").value = data.emailAddress;
-		document.getElementById("department").value = data.department;
 		document.getElementById("employeeCode").value = data.employeeCode;
+		document.getElementById("firstName").value = data.firstName;
+		document.getElementById("department").value = data.department;
 		document.getElementById("jobTitleName").value = data.jobTitleName;
+		document.getElementById("phoneNumber").value = data.phoneNumber;
+		document.getElementById("emailAddress").value = data.emailAddress;
+		document.getElementById("password").value = data.password;
+		
+		
+		
 	})
 }
 
 
 function postData() {
 	
-	var userId = document.getElementById("userId").value;
+        var employeeCode = document.getElementById("employeeCode").value;
 	var firstName = document.getElementById("firstName").value;
-	
-	
-	var lastName = document.getElementById("lastName").value;
-	var phoneNumber = document.getElementById("phoneNumber").value;
-	
-	var emailAddress = document.getElementById("emailAddress").value;
 	var department = document.getElementById("department").value;
-	
-	var employeeCode = document.getElementById("employeeCode").value;
 	var jobTitleName = document.getElementById("jobTitleName").value;
+	var phoneNumber = document.getElementById("phoneNumber").value;
+	var emailAddress = document.getElementById("emailAddress").value;
+	var password = document.getElementById("password").value;
 	
-	data = {userId: userId, firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, emailAddress: emailAddress, department: department, employeeCode: employeeCode, jobTitleName: jobTitleName};
+	
+	
+	
+	
+	data = {firstName: firstName, phoneNumber: phoneNumber, emailAddress: emailAddress, department: department, employeeCode: employeeCode, jobTitleName: jobTitleName,password:password};
 	
 	fetch(api_url, {
 		method: "POST",
@@ -92,23 +91,17 @@ function postData() {
 
 function putData() {
 	
-	var _id = document.getElementById("id").value;
-
-	var userId = document.getElementById("userId").value;
-	var jobTitleName = document.getElementById("jobTitleName").value;
-	var firstName = document.getElementById("firstName").value;
-	var lastName = document.getElementById("lastName").value;
-	
-	
-	
-	var department = document.getElementById("department").value;
 	var employeeCode = document.getElementById("employeeCode").value;
-	var emailAddress = document.getElementById("emailAddress").value;
+	var firstName = document.getElementById("firstName").value;
+	var department = document.getElementById("department").value;
+	var jobTitleName = document.getElementById("jobTitleName").value;
 	var phoneNumber = document.getElementById("phoneNumber").value;
+	var emailAddress = document.getElementById("emailAddress").value;
+	var password = document.getElementById("password").value;
 	
 	
 	
-	data = {_id:_id,userId:userId, jobTitleName: jobTitleName, firstName: firstName, lastName: lastName, department: department, employeeCode: employeeCode, emailAddress: emailAddress, phoneNumber: phoneNumber};
+	data = {_id:_id, jobTitleName: jobTitleName, firstName: firstName,password:password, department: department, employeeCode: employeeCode, emailAddress: emailAddress, phoneNumber: phoneNumber};
 	
 	
 	
